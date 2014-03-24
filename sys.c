8ae9ab94bffadd28cb06dcaec9848e89f4c97527
griera <genis.riera.perez@gmail.com>
2014-03-24 09:10:21 +0100

    Compiles with current implementation of fork(). This syscall must still be tested so hard

--- sys.c
+++ sys.c
@@ -46,7 +46,7 @@ int sys_fork()
     if (list_empty(&freequeue)) return -EAGAIN;
     
     /* Needed variables related to child and father processes */
-    struct list_head *free_pcb = list_first(freequeue);
+    struct list_head *free_pcb = list_first(&freequeue);
     union task_union *child = (union task_union*)list_head_to_task_struct(free_pcb);
     union task_union *parent = (union task_union *)current();
     struct task_struct *pcb_child = &(child->task); 
@@ -92,9 +92,9 @@ int sys_fork()
     /* Inherit user data */
     for (i = 0; i < NUM_PAG_DATA; i++) {
         unsigned int logic_addr = (i + PAG_LOG_INIT_DATA_P0) * PAGE_SIZE;
-        set_ss_pag(pagt_father, i + PAG_LOG_INIT_DATA_P0 + NUM_PAG_DATA, resv_frames[i]);
+        set_ss_pag(pagt_parent, i + PAG_LOG_INIT_DATA_P0 + NUM_PAG_DATA, resv_frames[i]);
         copy_data((void *)logic_addr, (void *)(logic_addr + PAGE_SIZE * NUM_PAG_DATA), PAGE_SIZE);
-        del_ss_pag(pagt_father, i + PAG_LOG_INIT_DATA_P0 + NUM_PAG_DATA);
+        del_ss_pag(pagt_parent, i + PAG_LOG_INIT_DATA_P0 + NUM_PAG_DATA);
     }
 
     /* Flushes entire TLB */
@@ -102,7 +102,7 @@ int sys_fork()
 
     /* Updates child's PCB */
     PID = new_pid();
-    pcb_child->pid = PID
+    pcb_child->PID = PID;
     pcb_child->quantum = DEFAULT_QUANTUM;
 
     /* TODO: Prepare child's kernel stack to perform context switch */
@@ -118,10 +118,10 @@ int sys_fork()
      * (see documentation provided by entry.S file to know the kernel stack
      * status when switches to privilege level 0
      */
-    pcb_child->stack[KERNEL_STACK_SIZE-10] = 0;
+    child->stack[KERNEL_STACK_SIZE - 10] = 0;
 
     /* Adds child process to ready queue and returns its PID from parent */
-    list_add_tail(&(child_pcb->list), &readyqueue);
+    list_add_tail(&(pcb_child->list), &readyqueue);
     return PID;
 }
 
