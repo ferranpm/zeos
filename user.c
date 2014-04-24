a9f77bf9056b631fde333ecbd8a754bec635c5b8
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-04-24 18:00:31 +0200

    Test del clone

--- user.c
+++ user.c
@@ -3,20 +3,29 @@
 char buff[24];
 int pid;
 
-void func_clone() {
-    char *s = "Hola soc el clone\n";
-    write(1, s, strlen(s));
-    exit(-1);
-}
-
-int pila[1000];
-
 int __attribute__ ((__section__(".text.main"))) main(void)
 {
     /* Run a test suite provided by lab course */
     // runjp_rank(0, 28);
 
-    clone(func_clone, &pila[1000]);
+    char *fill = "FILL\n";
+    char *pare = "PARE\n";
+    int i;
+
+    sem_init(0, 0);
+    if (fork() == 0) {
+        for (i = 0; i < 20; i++) {
+            write(1, fill, strlen(fill));
+        }
+        sem_signal(0);
+    }
+    else {
+        sem_wait(0);
+        for (i = 0; i < 20; i++) {
+            write(1, pare, strlen(pare));
+        }
+    }
+
     while (1);
 
     return 0;
