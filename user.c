f172d0e2a76475322788b3ab790dff37d9f50d28
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2014-04-24 17:15:56 +0200

    Test de clone

--- user.c
+++ user.c
@@ -3,11 +3,20 @@
 char buff[24];
 int pid;
 
+void func_clone() {
+    char *s = "Hola soc el clone\n";
+    write(1, s, strlen(s));
+    exit(-1);
+}
+
+int pila[1000];
+
 int __attribute__ ((__section__(".text.main"))) main(void)
 {
     /* Run a test suite provided by lab course */
-    runjp_rank(0, 28);
+    // runjp_rank(0, 28);
 
+    clone(func_clone, &pila[1000]);
     while (1);
 
     return 0;
